239269e873a9a00384cae8bcb44fd791d73998d8
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2025-06-08 00:11:12 +0530

    Basic setup completed

---
+++ src/main/java/com/interview/SpringbootInterview2025Application.java
@@ -0,0 +1,13 @@
+package com.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterview2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInterview2025Application.class, args);
+    }
+
+}
